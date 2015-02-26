9b31d309affd707cbd7875473deb2535f3eb5fb5
Catherine dewerd <cdewerd@gmail.com>
2015-02-26 16:47:06 -0700

    Got skeletons of all necessary pages
    Remove and put still don’t function from the webpage

--- breakfast/routes/index.js
+++ breakfast/routes/index.js
@@ -59,13 +59,13 @@ router.post('/adduser', function(req, res) {
 });
 
 /* GET Update User page. */
-router.update('/updateuser', function(req, res) {
+router.get('/updateuser', function(req, res) {
     res.render('updateuser', { title: 'Add New User' });
 });
 
 
 /*PUT modify user, used to change ratings */
-router.update('/updateuser', function(req, res) {
+router.put('/putuser', function(req, res) {
 
     // Set our internal DB variable
     var db = req.db;
@@ -78,10 +78,9 @@ router.update('/updateuser', function(req, res) {
     var collection = db.get('usercollection');
 
     // Submit to the DB
-    collection.update({
-        "breakfast" : breakFast},
-        {$set:{"rating": raTing}}
-
+    collection.update(
+        {breakfast : breakFast},
+        {$set:{rating: raTing}
     }, function (err, doc) {
         if (err) {
             // If it failed, return error
@@ -94,15 +93,46 @@ router.update('/updateuser', function(req, res) {
             res.redirect("userlist");
         }
     });
+});
 
 
 
 
+/* GET home page. */
+router.get('/removeuser', function(req, res, next) {
+  res.render('removeuser', { title: 'Express' });
+});
 
 
-/*DELETE removes an entry */
+/*REMOVE removes an entry */
+router.delete('/deleteuser', function(req, res) {
 
+    // Set our internal DB variable
+    var db = req.db;
 
+    // Get our form values. These rely on the "name" attributes
+    var breakFast = req.body.breakfast;
+    //var raTings = req.body.rating;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.remove({
+        breakfast: breakFast}
+	, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
 
 
 
